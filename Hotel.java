e08c5f6637273cf5f85eed8369c68f1da6bb96a2
Sagarsagar27 <sagarchirag456@gmail.com>
2025-08-08 16:13:06 +0530

    java program

---
+++ Hotel.java
@@ -0,0 +1,20 @@
+class Hotel{
+public static void main(String[] args){
+ byte hotelNumber=45;
+ short addressNumber=23;
+ int price=54254;
+ long contact=7854754542l;
+ char series='z';
+ float gst=1234.3456f;
+ double sgst=12345.234556788d;
+ boolean isAvailable=true;
+System.out.println("the hotel number is: "+hotelNumber);
+System.out.println("the address of hotel is: "+addressNumber);
+System.out.println("the hotel price is: "+price);
+System.out.println("the contactis: "+contact);
+System.out.println("the hotel series is: "+series);
+System.out.println("the hotel gst is: "+gst);
+System.out.println("the hotel sgst is: "+sgst);
+System.out.println("the hotel isAvailable: "+isAvailable);
+}
+}
