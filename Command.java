7fa6e71b5d0aba26d8569a7c773ee2849d2ac187
Sagarcr21 <sagarcr.xworkz@gmail.com>
2025-08-11 15:09:17 +0530

    java program

---
+++ Command.java
@@ -0,0 +1,11 @@
+git status
+git add filename.java
+git status
+git commit -m "message"
+git push origin branchname
+git config --global user.email "sagarcr.xworkz@gmail.com"
+git config --global user.name "sagarcr21"
+
+
+local<-----------------------remote
+           git clone url
---
+++ Friends.java
@@ -0,0 +1,12 @@
+class Friends{
+	public static void main(String[] args){
+		System.out.println("sagar");
+		System.out.println("chirag");
+		System.out.println("Prajwal");
+		System.out.println("vijay");
+		System.out.println("harshan");
+		
+	}
+
+
+}
