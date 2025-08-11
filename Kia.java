3a25f8f6dafe6ac068a1ed639f2f81acc294ebaf
sagarcr21 <sagarcr.xworkz@gmail.com>
2025-08-11 15:39:15 +0530

    java program

---
+++ Kia.java
@@ -0,0 +1,24 @@
+class Kia{
+public static void main(String[] args){
+byte carSeater=5;
+short model=31875;
+int price=120000;
+int disel=50;
+long engineNumber=4567867678l;
+long contact=9611605856l;
+char series='x';
+float gst=12345.45648f;
+double sgst=12345.3455678765d;
+boolean isAvailable=true;
+System.out.println("the car seater is: "+carSeater);
+System.out.println("the model is: "+model);
+System.out.println("the car price is: "+price);
+System.out.println("the car disel in litre is: "+disel);
+System.out.println("the car engineNumber is: "+engineNumber);
+System.out.println("the contactis: "+contact);
+System.out.println("the car series is: "+series);
+System.out.println("the car gst is: "+gst);
+System.out.println("the car sgst is: "+sgst);
+System.out.println("the car isAvailable: "+isAvailable);
+}
+}
