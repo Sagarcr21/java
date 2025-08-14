7f5b75e353a45f08038cee15ab0ac51a4c18df6d
Sagarcr21 <sagarcr.xworkz@gmail.com>
2025-08-14 15:04:55 +0530

    Add files via upload

---
+++ Carfeatures.java
@@ -0,0 +1,17 @@
+class Carfeatures{
+public static void main(String[] args){
+   String [] kiaCar={"petrol","fourseater","wheels","classic","interior"};
+   String [] hondaCar={"disel","fiveseater","wheels","safety","interior"};
+   String [] curvCar={"electric","fourseater","wheels","classic","interior"};
+   String [] cretaCar={"petrol","fourseater","wheels","safety","interior"};
+   String [] innovaCar={"disel","sevenseater","wheels","classic","interior"};
+   System.out.println("the kia car features  is ;"+kiaCar[3]);
+   System.out.println("the honda car features is ;"+hondaCar[3]);
+   System.out.println("the curv car features is ;"+curvCar[3]);
+   System.out.println("the creta car features is ;"+cretaCar[3]);
+   System.out.println("the innova car features is :"+innovaCar[3]);
+   }
+   }
+	
+	
+	
