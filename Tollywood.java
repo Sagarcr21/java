052a0787b6e7108e50070aef6625deb07fff4eae
Sagarcr21 <sagarcr.xworkz@gmail.com>
2025-08-18 14:48:24 +0530

    Add files via upload

---
+++ Tollywood.java
@@ -0,0 +1,21 @@
+class Tollywood{ 
+public static void main(String[] args){
+  int i;
+  String tollywoodMovies[]={"bahubali","peddi","rrr","sita ramam","puspa"};
+    for(i=0;i<tollywoodMovies.length;i++){
+	System.out.println( "the tollywood movies is :"+tollywoodMovies[i]);
+	}
+	/*
+//	for loop reverse
+    for(i=tollywoodMovies.length-1;i>0;i--){
+	System.out.println(" the tollywoodMoviesis :"+tollywoodMovies[i]);
+	}
+	
+//for each statement
+  for(String movie: tollywoodMovies){
+  System.out.println("tollywoodMoviesis:"+movie);
+}  
+*/
+	
+	}
+	}
