216c95c1b43354b146887b692320d57a0363bff3
Sagarcr21 <sagarcr.xworkz@gmail.com>
2025-08-18 14:49:35 +0530

    Add files via upload

---
+++ India.java
@@ -0,0 +1,21 @@
+class India{ 
+public static void main(String[] args){
+  int i;
+  String statesofIndia[]={"karnataka","goa","kerela","tamilnadu","maharastra"};
+  /*  for(i=0;i<statesofIndia.length;i++){
+	System.out.println( "the statesofIndia is :"+statesofIndia[i]);
+	}
+	*/
+//	for loop reverse
+    for(i=statesofIndia.length-1;i>0;i--){
+	System.out.println(" the statesofIndiais :"+statesofIndia[i]);
+	}
+	
+//for each statement
+  /*for(String state: statesofIndia){
+  System.out.println("statesofIndia:"+state);
+}  
+*/
+	
+	}
+	}
