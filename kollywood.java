7b2cf34e1a9a56a45e0f1465107bd6fa9d6ef259
Sagarcr21 <sagarcr.xworkz@gmail.com>
2025-08-18 14:47:43 +0530

    Add files via upload

---
+++ kollywood.java
@@ -0,0 +1,21 @@
+class Kollywood{
+	static int i;
+  public static void main(String [] args){
+	  //int i;
+    String kollywoodMovies[]={"beast","coolie","leo","amraan","vikram","jailer"};
+	for(i=0;i<kollywoodMovies.length;i++){
+	System.out.println(" the kollywoodMovies is:"+kollywoodMovies[i]);
+  }
+  /*for loop(reverse)
+  for(i=kollywoodMovies.length-1;i>0;i--){
+	System.out.println(" the kollywoodMovies is:"+kollywoodMovies[i]);
+  }
+	
+   for each statement
+ for (String movie : kollywoodMovies) {
+           System.out.println( "the kollywood movies is :"+movie);
+        }
+		*/
+		}
+}
+
