d15d49061acdea819188d9439292ec0a080d2d6f
sagarcr21 <sagarcr.xworkz@gmail.com>
2025-09-03 11:19:33 +0530

    method class

---
+++ Prajwal.java
@@ -0,0 +1,24 @@
+class Prajwal{
+public static void wentCollege(){
+System.out.println("prajwal will be going to college");
+}
+public static void enterClass(){
+System.out.println("prajwal entered the class");
+}
+public static void writtenExam(){
+System.out.println("prajwal had written the exam");
+}
+public static void discuusFriends(){
+System.out.println("prajwal was discusswd about the exam");
+}
+public static void returnBack(){
+System.out.println("prajwal was return back to home");
+}
+public static void main(String[] args){
+wentCollege();
+enterClass();
+writtenExam();
+discuusFriends();
+returnBack();
+}
+}
