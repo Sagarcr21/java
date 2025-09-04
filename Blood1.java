e4e6af27bef3c03ed68e28d71687a4e155be06fb
sagarcr21 <sagarcr.xworkz@gmail.com>
2025-09-04 16:14:25 +0530

    testing

---
+++ Blood1.java
@@ -0,0 +1,11 @@
+class Blood {
+	public static void main(String[] args){
+		System.out.println("person name, blood group");
+		System.out.println("sagar,B+ve,");
+		System.out.println("kumar,O+ve");
+		
+		
+	}
+
+
+}
